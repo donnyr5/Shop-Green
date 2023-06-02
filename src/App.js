f5ea5754d9ee7fe797ce33c0d8c3f18e7c1d8f52
Donny Rimer <donnyjrimer@gmail.com>
2023-06-01 21:02:39 -0700

    marketplace and posting item space are now separate

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import './App.css';
 import ListItems from './components/ListItems';
 import AddItem from './components/AddItem';
+import React, {useState} from 'react';
 
 function App() {
-  return (
-    <div className='App'>
-      <main>
-        <ListItems />
-        <AddItem />
-      </main>
-    </div>
 
+  const [isPosting, setIsPosting] = useState(false)
+
+  //changes the boolean to the opposite.
+  function toggleIsPosting(){
+    setIsPosting(!isPosting)
+  }
+
+  function Marketplace(props){
+    return (
+      <div className='App'>
+        <main>
+          <ListItems />
+          <button onClick={props.toggleIsPosting}>Sell an Item</button>
+        </main>
+      </div>
+    )
+  }
+
+  function PostingItem(props){
+    return (
+      <div className='App'>
+        <main>
+          <button onClick={props.toggleIsPosting}>Go Back</button>
+          <AddItem />
+        </main>
+      </div>
+    )
+  }
+
+  //this is if the user is logged in.
+  return (
+    isPosting ? <PostingItem toggleIsPosting={toggleIsPosting} /> : <Marketplace toggleIsPosting={toggleIsPosting} />
   )
 }
 
